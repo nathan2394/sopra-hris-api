9b220fa17a96e40700e58a7178a8e7d88cb55b11
it-solusipack <it_pm@solusi-pack.com>
2025-06-19 16:21:52 +0700

    fix controller departments and dashboard

--- src/Controllers/DashboardController.cs
+++ src/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using sopra_hris_api.Entities;
 using sopra_hris_api.Responses;
+using sopra_hris_api.src.Entities;
 using sopra_hris_api.src.Services;
 
 namespace sopra_hris_api.Controllers;
@@ -11,22 +12,21 @@ namespace sopra_hris_api.Controllers;
 [ApiController]
 [Route("[controller]")]
 [Authorize]
-public class DepartmentsController : ControllerBase
+public class DashboardController : ControllerBase
 {
-    private readonly IServiceAsync<Departments> _service;
+    private readonly IServiceDashboardAsync<DashboardDTO> _service;
 
-    public DepartmentsController(IServiceAsync<Departments> service)
+    public DashboardController(IServiceDashboardAsync<DashboardDTO> service)
     {
         _service = service;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> Get(int limit = 0, int page = 0, string search = "", string sort = "", string filter = "", string date = "")
+    [HttpGet("Approval")]
+    public async Task<IActionResult> GetApproval(string filter = "", string date = "")
     {
         try
         {
-            var total = 0;
-            var result = await _service.GetAllAsync(limit, page, total, search, sort, filter, date);
+            var result = await _service.GetApproval(filter, date);
             return Ok(result);
         }
         catch (Exception ex)
@@ -41,18 +41,13 @@ public class DepartmentsController : ControllerBase
             return BadRequest(new { message });
         }
     }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(int id)
+    [HttpGet("summary")]
+    public async Task<IActionResult> GetAttendanceSummary(string filter = "", string date = "")
     {
         try
         {
-            var result = await _service.GetByIdAsync(id);
-            if (result == null)
-                return BadRequest(new { message = "Invalid ID" });
-
-            var response = new Response<Departments>(result);
-            return Ok(response);
+            var result = await _service.GetAttendanceSummary(filter, date);
+            return Ok(result);
         }
         catch (Exception ex)
         {
@@ -63,21 +58,16 @@ public class DepartmentsController : ControllerBase
                 message = inner.Message;
                 inner = inner.InnerException;
             }
-            Trace.WriteLine(message, "DepartmentsController");
             return BadRequest(new { message });
         }
     }
-
-    [HttpPost]
-    public async Task<IActionResult> Create([FromBody] Departments obj)
+    [HttpGet("AttendanceNormalAbnormal")]
+    public async Task<IActionResult> GetAttendanceNormalAbnormal(string filter = "", string date = "")
     {
         try
         {
-            obj.UserIn = Convert.ToInt64(User.FindFirstValue("id"));
-
-            var result = await _service.CreateAsync(obj);
-            var response = new Response<Departments>(result);
-            return Ok(response);
+            var result = await _service.GetAttendanceNormalAbnormal(filter, date);
+            return Ok(result);
         }
         catch (Exception ex)
         {
@@ -88,46 +78,16 @@ public class DepartmentsController : ControllerBase
                 message = inner.Message;
                 inner = inner.InnerException;
             }
-            Trace.WriteLine(message, "DepartmentsController");
-            return BadRequest(new { message });
-        }
-
-    }
-
-    [HttpPut]
-    public async Task<IActionResult> Edit([FromBody] Departments obj)
-    {
-        try
-        {
-            obj.UserUp = Convert.ToInt64(User.FindFirstValue("id"));
-
-            var result = await _service.EditAsync(obj);
-            var response = new Response<Departments>(result);
-            return Ok(response);
-        }
-        catch (Exception ex)
-        {
-            var message = ex.Message;
-            var inner = ex.InnerException;
-            while (inner != null)
-            {
-                message = inner.Message;
-                inner = inner.InnerException;
-            }
-            Trace.WriteLine(message, "DepartmentsController");
             return BadRequest(new { message });
         }
     }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
+    [HttpGet("AttendanceByShift")]
+    public async Task<IActionResult> GetAttendanceByShift(string filter = "", string date = "")
     {
         try
         {
-            var result = await _service.DeleteAsync(id, Convert.ToInt64(User.FindFirstValue("id")));
-
-            var response = new Response<object>(result);
-            return Ok(response);
+            var result = await _service.GetAttendanceByShift(filter, date);
+            return Ok(result);
         }
         catch (Exception ex)
         {
@@ -138,7 +98,26 @@ public class DepartmentsController : ControllerBase
                 message = inner.Message;
                 inner = inner.InnerException;
             }
-            Trace.WriteLine(message, "DepartmentsController");
+            return BadRequest(new { message });
+        }
+    }
+    [HttpGet("BudgetOvertimes")]
+    public async Task<IActionResult> GetBudgetOvertimes(string filter = "", string date = "")
+    {
+        try
+        {
+            var result = await _service.GetBudgetOvertimes(filter, date);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            var message = ex.Message;
+            var inner = ex.InnerException;
+            while (inner != null)
+            {
+                message = inner.Message;
+                inner = inner.InnerException;
+            }
             return BadRequest(new { message });
         }
     }
