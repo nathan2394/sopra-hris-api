539f70cd3e0c3949914bcfc4f8cdbcee004c383e
abhisanna <70308024+abhisanna@users.noreply.github.com>
2026-04-02 15:38:51 +0700

    {+} User Companies

--- src/Entities/Company.cs
+++ src/Entities/Company.cs
@@ -9,6 +9,18 @@ namespace sopra_hris_api.Entities
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long CompanyID { get; set; }
-        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? Name { get; set; }
+        public string? ApiLink { get; set; }
+        public string? LogoPath { get; set; }
+    }
+
+    public class CompanyDto
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Code { get; set; }
+        public string? Company { get; set; }
+        public string? ApiLink { get; set; }
+        public string? LogoPath { get; set; }
     }
 }
